e81ee98f4dd7458ce3ccba42d045e680051f8689
Agent BUB <zzhines@gmail.com>
2025-04-08 07:16:45 -0400

    Plugin Outlet Update

---
+++ javascript/discourse/helpers/category-filters.js
@@ -0,0 +1,27 @@
+import { registerUnbound } from "discourse-common/lib/helpers";
+
+registerUnbound("filter-core", (categories) =>
+  categories.filter((a) => a.slug.match(/^bacta-/))
+);
+
+// registerUnbound("filter-interstate", ...)
+
+registerUnbound("filter-togr", (categories) =>
+  categories.filter((a) => a.slug.match(/^togr-/i))
+);
+
+registerUnbound("filter-apps-appeals", (categories) =>
+  categories.filter((a) => /^app-|^appeal-/.test(a.slug))
+);
+
+registerUnbound("filter-dev", (categories) =>
+  categories.filter((a) => a.slug.match(/^dev-/))
+);
+
+registerUnbound("filter-private", (categories) =>
+  categories.filter((a) => /^forums-staff|^staff-/.test(a.slug))
+);
+
+registerUnbound("filter-closed", (categories) =>
+  categories.filter((a) => /^closed-|^scp-|^interstate-/.test(a.slug))
+);
