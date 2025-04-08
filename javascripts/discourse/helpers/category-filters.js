a19e96398cc473ba48676775fc3a0fd8888c507a
Agent BUB <zzhines@gmail.com>
2025-04-08 07:38:46 -0400

    Fix

--- javascripts/discourse/helpers/category-filters.js
+++ javascripts/discourse/helpers/category-filters.js
@@ -1,25 +1,33 @@
 import { registerUnbound } from "discourse-common/lib/helpers";
 
-registerUnbound("filter-core", (categories) =>
-  categories.filter((a) => a.slug.match(/^bacta-/))
-);
+registerUnbound("filter-core", (categories) => {
+  return categories?.filter((a) => a.slug?.match(/^bacta-/));
+});
 
-registerUnbound("filter-togr", (categories) =>
-  categories.filter((a) => a.slug.match(/^togr-/i))
-);
+registerUnbound("filter-togr", (categories) => {
+  return categories?.filter((a) => a.slug?.match(/^togr-/i));
+});
 
-registerUnbound("filter-apps-appeals", (categories) =>
-  categories.filter((a) => /^app-|^appeal-/.test(a.slug))
-);
+registerUnbound("filter-apps-appeals", (categories) => {
+  return categories?.filter((a) =>
+    a.slug?.match(/^app-/) || a.slug?.match(/^appeal-/)
+  );
+});
 
-registerUnbound("filter-dev", (categories) =>
-  categories.filter((a) => a.slug.match(/^dev-/))
-);
+registerUnbound("filter-dev", (categories) => {
+  return categories?.filter((a) => a.slug?.match(/^dev-/));
+});
 
-registerUnbound("filter-private", (categories) =>
-  categories.filter((a) => /^forums-staff|^staff-/.test(a.slug))
-);
+registerUnbound("filter-private", (categories) => {
+  return categories?.filter((a) =>
+    a.slug?.match(/^forums-staff/) || a.slug?.match(/^staff-/)
+  );
+});
 
-registerUnbound("filter-closed", (categories) =>
-  categories.filter((a) => /^closed-|^scp-|^interstate-/.test(a.slug))
-);
+registerUnbound("filter-closed", (categories) => {
+  return categories?.filter((a) =>
+    a.slug?.match(/^closed-/i) ||
+    a.slug?.match(/^scp-/) ||
+    a.slug?.match(/^interstate-/)
+  );
+});
